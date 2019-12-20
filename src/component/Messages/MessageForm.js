d1e82a3aa8e324103e061057bf0465a8bdb634a1
Vijaya Kumar V <vijayvulchi@users.noreply.github.com>
2019-12-20 15:18:15 +0530

    Adding / Uploading Messages with Media Files

--- src/component/Messages/MessageForm.js
+++ src/component/Messages/MessageForm.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import uuidv4 from 'uuid/v4';
 import firebase from './../../firebase';
 import { Segment, Input, Button } from 'semantic-ui-react';
+
+import FileModal from './FileModal';
+import ProgressBar from './ProgressBar';
 
 class MessageForm extends React.Component {
 
@@ -9,16 +13,23 @@ class MessageForm extends React.Component {
 		loading: false,
 		channel: this.props.currentChannel,
 		user: this.props.currentUser,
-		errors: []
+		errors: [],
+		modal: false,
+		uploadState: '',
+		uploadTask: null,
+		storageRef: firebase.storage().ref(),
+		percentUpload: 0
 	}
+
+	openModal = () => this.setState({ modal: true });
+	closeModal = () => this.setState({ modal: false });
 
 	handleChange = event => {
 		this.setState({[event.target.name]: event.target.value });
 	}
 
-	createMessage = () => {
+	createMessage = (fileUrl = null) => {
 		const message = {
-			content: this.state.message,
 			timestamp: firebase.database.ServerValue.TIMESTAMP,
 			user: {
 				id: this.state.user.uid,
@@ -26,6 +37,11 @@ class MessageForm extends React.Component {
 				avatar: this.state.user.photoURL
 			}
 		};
+		if (fileUrl !== null) {
+			message['image'] = fileUrl;
+		} else {
+			message['content'] = this.state.message;
+		}
 		return message;
 	}
 
@@ -55,9 +71,61 @@ class MessageForm extends React.Component {
 			})
 		}
 	}
+
+	uploadFile = (file, metadata) => {
+		const pathToUpload = this.state.channel.id;
+		const ref = this.props.messagesRef;
+		const filePath = `chat/public/${uuidv4()}.jpg`;
+
+		this.setState({
+			uploadState: 'uploading',
+			uploadTask: this.state.storageRef.child(filePath).put(file, metadata)
+		}, () => {
+			this.state.uploadTask.on('state_changed', snap => {
+				const percentUpload = ((snap.bytesTransferred / snap.totalBytes) * 100);
+				this.setState({ percentUpload });
+			}, err => {
+					console.error(err);
+					this.setState({
+						errors: this.state.errors.concat(err),
+						uploadState: 'error',
+						uploadTask: null
+					})
+				}, () => {
+					this.state.uploadTask.snapshot.ref.getDownloadURL().then(downloadUrl => {
+						this.sendFileMessage(downloadUrl, ref, pathToUpload);
+					})
+					.catch(err => {
+						console.error(err);
+						this.setState({
+						errors: this.state.errors.concat(err),
+							uploadState: 'error',
+							uploadTask: null
+						})
+					})
+					}
+				)
+			}
+		)
+	}
 	
+	sendFileMessage = (fileUrl, ref, pathToUpload) => {
+		ref.child(pathToUpload)
+			.push()
+			.set(this.createMessage(fileUrl))
+			.then(() => {
+				this.setState({ uploadState: 'done'})
+			})
+			.catch(err => {
+				console.error(err);
+				this.setState({
+					errors: this.state.errors.concat(err)
+				})
+			})
+	}
+
 	render() {
-		const { errors, message, loading } = this.state;
+		const { errors, message, loading, modal, uploadState, percentUpload } = this.state;
 
 		return (
 			<Segment className="message__form">
@@ -85,11 +153,21 @@ class MessageForm extends React.Component {
 					/>
 					<Button
 						color="teal"
+						onClick={this.openModal}
 						content="upload Media"
 						labelPosition="right"
 						icon="cloud upload"
 					/>
 				</Button.Group>
+				<FileModal
+					modal={modal}
+					closeModal={this.closeModal}
+					uploadFile={this.uploadFile}
+				/>
+				<ProgressBar
+					uploadState={uploadState}
+					percentUpload={percentUpload}
+				/>
 			</Segment>
 		)
 	}
---
+++ src/component/Messages/ProgressBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Progress } from 'semantic-ui-react';
+
+const ProgressBar = ({ uploadState, percentUpload }) => (
+	uploadState === "uploading" && (
+		<Progress
+			className="progress__bar"
+			percent={percentUpload}
+			progress
+			indicating
+			size="tiny"
+			inverted
+		/>
+	)
+);
+
+export default ProgressBar;
